2a86784ed697054dfb73631974a8f5a62f798cde
md tarik bosunia <bosuniamdtarik005@gmail.com>
2024-02-15 13:20:15 +0600

    start c programming

---
+++ c programming/1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    // print my name
+    printf("a\n");
+
+    // string "e"
+
+    // character 'a'
+
+    /*
+        this is
+        multi line
+        comment
+    
+    */
+   printf("gfgagf af as");
+
+    return 0;
+}
